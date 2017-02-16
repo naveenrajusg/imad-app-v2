002a04e459334e8f5499829d0a0842143f8d2e28
naveenrajusg <naveenraju100@gmail.com>
2017-02-17 00:42:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,42 +21,42 @@ content:` <p>
              This is my content for the first artical. This is my content for the first artical.
              This is my content for the first artical. This is my content for the first artical.
            </p>`
-}
+};
+fuction createTemplate (data){
+var title = data.title;
+var data  = data.data;
+var heading=data.heading;
+var content=data.content;
 var htmltemplate=`
-
-    <html>
-     <head>
-    <title>
-      artical_one | Naveen Raju
-    </title>
-    <meta name="viewport" content="width-device-width,initial-scale=1"/>
-    <link href="/ui/style.css" rel="stylesheet" />
-  </head>
-  <body>
-    <div class="container">
-        <div>
-          <d href="/">Home</d>
-        </div>
-        <hr/>
-        <h3>
-          Artical_one
-        </h3>
-        <div>
-          ${date}
-        </div>
-         <div>
-           ${content}
-         </div>
-     </div>
-  </body>
-</html>
-
-
-
-
-
-
-`;
+                 <html>
+                 <head>
+                <title>
+                  ${title}
+                </title>
+                 <meta name="viewport" content="width-device-width,initial-scale=1"/>
+                <link href="/ui/style.css" rel="stylesheet" />
+              </head>
+              <body>
+                <div class="container">
+                    <div>
+                      <d href="/">Home</d>
+                    </div>
+                    <hr/>
+                    <h3>
+                      ${heading}
+                    </h3>
+                    <div>
+                      ${date}
+                    </div>
+                     <div>
+                       ${content}
+                     </div>
+                 </div>
+              </body>
+            </html>
+            `;
+            return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
