36738af948e1152a9bd0ef8653f11de79417564f
naveenrajusg <naveenraju100@gmail.com>
2017-02-16 22:16:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('artical_one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'artical_one.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'article_one.html'));
 });
 
 
